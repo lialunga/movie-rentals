adb9a289ad19025e4f6fa27cd0df33771633e553
plibapassil <passilpaulino@gmail.com>
2023-03-20 14:07:18 +0100

    feat: adiciona documentação com swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express"
 import { routes } from "./routes";
 
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from "./swagger.json"
+
 const app = express();
 
 app.use(express.json())
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(routes)
 
