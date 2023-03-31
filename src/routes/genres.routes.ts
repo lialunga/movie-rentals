6c17cc88ace12a177fe4795999b6bab7d36f63d6
plibapassil <passilpaulino@gmail.com>
2023-03-31 16:19:05 +0100

    feat: adiciona rota que lista todos os gêneros

--- src/routes/genres.routes.ts
+++ src/routes/genres.routes.ts
@@ -2,13 +2,17 @@ import { Router } from "express";
 import { ensureAuthenticated } from "../shared/middlewares/ensureAuthenticated";
 import { ensureAdmin } from "../shared/middlewares/ensureAdmin";
 import { CreateGenreController } from "../modules/genres/useCases/createGenre/CreateGenreController";
+import { ListGenreController } from "../modules/genres/useCases/listGenre/ListGenreController";
 
 
 const genresRoutes = Router()
 
 const createGenreController = new CreateGenreController();
+const listGenreController = new ListGenreController();
 
 genresRoutes.post("/", ensureAuthenticated, ensureAdmin, createGenreController.handle)
+
+genresRoutes.get("/", listGenreController.handle)
 
 
 export { genresRoutes }
