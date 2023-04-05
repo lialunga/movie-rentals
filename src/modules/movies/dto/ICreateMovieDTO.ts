23152cd0cbd6b397b088da286c6decfed1bfd275
plibapassil <passilpaulino@gmail.com>
2023-04-05 14:09:24 +0100

    feat: adiciona DTO de criação de filmes

---
+++ src/modules/movies/dto/ICreateMovieDTO.ts
@@ -0,0 +1,11 @@
+
+
+interface ICreateMoviesDTO {
+    name: string;
+    description: string;
+    daily_rate: number;
+    fine_amaunt: number;
+    genre_id: string;
+}
+
+export { ICreateMoviesDTO }
